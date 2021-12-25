7094bf0cf92a7f07dc3eb261df4570e7ecc18f10
Arina08 <76660949+Arina08@users.noreply.github.com>
2021-12-26 00:21:21 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,30 @@
 // реализация пользовательского приложения
+#include <arithmetic.h>
+
+using namespace std;
 
 int main()
 {
-  return 0;
+	string s;
+	for (int i = 0; i < 100; i++) {
+		try {
+			getline(cin, s);
+			isCorrect(s);
+			Arithmetic res;
+			res.stringToTerm(s);
+			res.print();
+			cout << endl;
+			res.termToPostfix();
+			res.print();
+			cout << endl;
+			double result = res.calculate();
+			cout << "Result: " << result;
+			cout << endl;
+		}
+		catch (char* smth) {
+			cout << smth << endl;
+		}
+		cout << endl;
+	}
+	return 0;
 }
